6b8bbb523dcefccff3dcf11f175bada833852416
Austin Warrick <austinwarrick@gmail.com>
2023-05-08 14:12:52 -0500

    fixed errors

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,16 @@ import { guestList } from "./Guests.js"
 const mainContainer = document.querySelector("#container")
 
 const applicationHTML = `
-<section class="services"
+<section class="services">
 
 </section>
 <div class="areas_and_guests">
-<section class="areas"
+<section class="areas">
 
 </section>
 <aside class="guestlist">
 ${guestList()}
-</section>
+</aside>
 </div>
 `
 
