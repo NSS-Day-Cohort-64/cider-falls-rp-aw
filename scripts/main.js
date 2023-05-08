e43a83ced7ca524f79e6b9161f093c80da3b1817
Ryan Phillips <ryanmphill@gmail.com>
2023-05-08 16:35:27 -0500

    Added event listener and exported function to build HTML list in services module. Imported funtion into main module.

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
 import { guestList } from "./Guests.js"
+import { serviceList } from "./Services.js"
 const mainContainer = document.querySelector("#container")
 
 const applicationHTML = `
 <section class="services">
-
+${serviceList()}
 </section>
 <div class="areas_and_guests">
 <section class="areas">
