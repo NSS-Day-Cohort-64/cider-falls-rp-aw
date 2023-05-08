451d0f5224f62082aa5cb07ae82664d46ce9c99f
Ryan Phillips <ryanmphill@gmail.com>
2023-05-08 14:06:52 -0500

    Added initial comments for services module

---
+++ scripts/Services.js
@@ -0,0 +1,10 @@
+// Import areas, sevices, supportedServices, and guests
+
+
+// Store a local copy of areas, services, supportedServices, and guests
+
+/* Export a function whose job it is to build an html list of all of the services offered.
+   The services will need data attributes for type, id, and name. */
+export const serviceList = () => {
+
+}
