2c72f1ce1d7151a7946f6466997351d91b8591a8
Austin Warrick <austinwarrick@gmail.com>
2023-05-09 09:06:57 -0500

    areas almost complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
-import { guestList } from "./Guests.js"
-const mainContainer = document.querySelector("#container")
+import { guestList } from "./Guests.js";
+import { areaList } from "./Areas.js";
+const mainContainer = document.querySelector("#container");
+
+
 
 const applicationHTML = `
 <section class="services">
@@ -7,12 +10,16 @@ const applicationHTML = `
 </section>
 <div class="areas_and_guests">
 <section class="areas">
-
+${areaList()}
 </section>
 <aside class="guestlist">
 ${guestList()}
 </aside>
 </div>
-`
+`;
+
+
+
+
 
 mainContainer.innerHTML = applicationHTML
